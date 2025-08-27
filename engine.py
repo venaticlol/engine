f62d08662cf4ec7e4cbee16635b6b43aa9fad4b0
venatic.gg <154387343+venaticlol@users.noreply.github.com>
2025-08-27 05:46:35 -0500

    Update engine.py

--- engine.py
+++ engine.py
@@ -843,11 +843,12 @@ def main():
         app.setWindowIcon(QIcon(icon_path))
 
     w = MainWindow()
-    w.resize(520, 675)
+    w.resize(520, 785)
     w.show()
 
     sys.exit(app.exec())
 
 if __name__ == "__main__":
     main()
+
 
