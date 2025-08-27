0e18f4d0b9fac0a9a7f9428ba16492bfbeb3f401
venatic.gg <154387343+venaticlol@users.noreply.github.com>
2025-08-27 05:45:13 -0500

    Update triggerbot.py

--- triggerbot.py
+++ triggerbot.py
@@ -888,10 +888,11 @@ def main():
         app.setWindowIcon(QIcon(icon_path))
 
     w = MainWindow()
-    w.resize(520, 620)
+    w.resize(520, 675)
     w.show()
 
     sys.exit(app.exec())
 
 if __name__ == "__main__":
     main()
+
